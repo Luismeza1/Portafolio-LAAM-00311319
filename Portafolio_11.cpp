59e87a3e0e296396cf6242941edeb32c9b68365e
Luismeza1 <00311319@uca.edu.sv>
2020-01-28 18:16:58 -0600

    Mod: P12

---
+++ Portafolio_11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*Reproduzca el ejemplo visto hoy pero agregue:
+Un campo que indique si es perecedero o no
+Agregue al menú:
+Mostrar solo articulos que tienen fecha de vencimiento
+Mostrar solo aquellos artıculos que pesan mas de 300 gramos.
+Mostrar solo aquellos artıculos que valen mas de 50 dolares.
+*/
+
+int main(void){
+
+    return 0;
+}
