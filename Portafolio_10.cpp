5ecbedd4527e5d81329c4084bfe6f0a2a2169517
Luismeza1 <00311319@uca.edu.sv>
2020-01-29 20:28:37 -0600

    Mod: P10

--- Portafolio_10.cpp
+++ Portafolio_10.cpp
@@ -79,6 +79,131 @@ int main(void){
         }
         
     }while(continuar);
-    
+     
     return 0;
 }
+
+void initialize(cola *q){
+    q->frente = NULL;
+    q->final = NULL;
+}
+
+bool empty(cola *q){
+    return (q->frente == NULL) ? true : false;
+}
+
+T front(cola *q){
+    if(empty(q)){
+        cout << "Underflow!" << endl;
+        return noValido;
+    }
+    return (q->frente)->elemento;
+}
+
+T back(cola *q){
+    if(empty(q)){
+        cout << "Underflow!" << endl;
+        return noValido;
+    }
+    return (q->final)->elemento;
+}
+
+void enqueue(cola *q, T x){
+    nodo *unNodo = new nodo;
+    unNodo->elemento = x;
+    unNodo->siguiente = NULL;
+    
+    if(q->final==NULL)
+        q->frente = unNodo;
+    else
+        (q->final)->siguiente = unNodo;
+        
+    q->final = unNodo;
+}
+
+T dequeue(cola *q){
+    if(empty(q)){
+        cout << "Underflow!" << endl;
+        return noValido;
+    }
+    nodo *unNodo = q->frente;
+    T x = unNodo->elemento;
+    q->frente = unNodo->siguiente;
+    if(q->frente==NULL)
+        q->final = NULL;
+    delete(unNodo);
+    return x;
+}
+
+int size(cola *q){
+    cola colaTemporal;
+    initialize(&colaTemporal);
+    
+    int contador = 0;
+    // Transfiriendo desde q hasta colaTemporal
+    while(!empty(q)){
+        T elemento = dequeue(q);
+        enqueue(&colaTemporal, elemento);
+        contador++;
+    }
+    // Transfiriendo desde colaTemporal hasta q
+    while(!empty(&colaTemporal)){
+        T elemento = dequeue(&colaTemporal);
+        enqueue(q, elemento);
+    }
+    return contador;
+}
+
+// Funciones 
+void agregar(){
+    // Solicitar datos del pedido
+    pedido unPedido;
+    cout << "Digite su apellido: "; getline(cin, unPedido.apellido);
+    cout << "Numero de la mesa: "; cin >> unPedido.mesa; cin.ignore();
+    cout << "Cantidad de tacos: "; cin >> unPedido.tacos; cin.ignore();
+    cout << "Cantidad de burritos: "; cin >> unPedido.burritos; cin.ignore();
+    cout << "Cantidad de tortas: "; cin >> unPedido.tortas; cin.ignore();
+    unPedido.tiempo = (unPedido.tacos+unPedido.burritos+unPedido.tortas) * 90;
+    
+    // Agregar a la cola
+    enqueue(&losPotros.colaPedidos, unPedido);
+    // Modificar contadores
+    losPotros.tosHechos += (unPedido.tacos);
+    losPotros.bHechos += (unPedido.burritos);
+    losPotros.tasHechos += (unPedido.tortas);
+}
+
+void consultar(){
+    cout << "Pedidos en espera: " << size(&losPotros.colaPedidos) << endl;
+    cout << "Total de tacos hechos: " << losPotros.tosHechos << endl;
+    cout << "Total de burritos hechos: " << losPotros.bHechos << endl;
+    cout << "Total de tortas hechas: " << losPotros.tasHechos << endl;
+}
+
+void servir(){
+    pedido pedidoDespachado = dequeue(&losPotros.colaPedidos);
+    cout << "Familia " << pedidoDespachado.apellido << " su pedido esta listo\n";
+    cout << "Se encuentran en la mesa " << pedidoDespachado.mesa << endl;
+}
+
+void vertodos(){
+    cola colaTemporal;
+    initialize(&colaTemporal);
+    
+    
+    while(!empty(&losPotros.colaPedidos)){
+        pedido unPedido = dequeue(&losPotros.colaPedidos);
+        enqueue(&colaTemporal, unPedido);
+        
+        cout << "Info del pedido: " << unPedido.apellido << ", ";
+        cout << "Tacos: " << unPedido.tacos << ", ";
+        cout << "Burritos: " << unPedido.burritos << ", ";
+        cout << "Tortas: " << unPedido.tortas << "\n\n";
+    }
+    
+    while(!empty(&colaTemporal)){
+        T elemento = dequeue(&colaTemporal);
+        enqueue(&losPotros.colaPedidos, elemento);
+    }
+}
+
