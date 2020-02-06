ed949e0c61e9c5bca43db4783af42adc093ef4d8
Luismeza1 <00311319@uca.edu.sv>
2020-02-06 00:13:05 -0600

    Mod: P_04C

--- Portafolio_04C.cpp
+++ Portafolio_04C.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 using namespace std;
 
-/*Buscar si elemento dado se encuentra en un arreglo (o no) de
-forma recursiva*/
+void encontrar(int *i, int *f, int n);
 
-int main(void){
-
+int main(void)
+{
+    int arr[10], numero;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Ingrese el valor " << i + 1 << " del arreglo: ";
+        cin >> arr[i];
+    }
+    cout << "\nIngrese el numero a buscar: ";
+    cin >> numero;
+    encontrar(arr, arr + 10, numero);
     return 0;
+}
+
+void encontrar(int *i, int *f, int n)
+{
+    if (i < f)
+    {
+        if (*i == n)
+        {
+            cout << *i << " concuerda con el numero buscado\n";
+            cout << "\nSe encontro el numero" << endl;
+        }
+        else
+        {
+            cout << *i << " no es igual a " << n << endl;
+            encontrar(i + 1, f, n);
+        }
+    }
+    else
+    {
+        cout << "\nNo se encontro el numero\n";
+    }
 }
