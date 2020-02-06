3c438cd43f6adb896f7d00138f7b21f90e031e4c
Luismeza1 <00311319@uca.edu.sv>
2020-02-06 00:30:20 -0600

    Mod:P_05

--- Portafolio_05.cpp
+++ Portafolio_05.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 using namespace std;
 
-/* Implementar el algoritmo de busqueda binaria
-Utilice el arreglo {1, 3, 4, 5, 17, 18, 31, 33}.
-Solicite al usuario un numero entero positivo.
-Busque dicho numero en el arreglo mediante una funcion recursiva
-que emplea la busqueda binaria.*/
-int main(void){
+int buscarA(int A[], int search, int high, int low);
 
+int main(){
+    int A[8] = {1, 3, 4, 5, 17, 18, 31, 33}, numero, pos;
+    
+    cout << "Ingrese el numero a buscar en el arreglo: ";
+    cin >> numero;
+    
+    pos = buscarA(A, numero,  7, 0);
+
+    if(pos == -1){
+        cout << "El numero no se encontro en el Arreglo\n";
+    }
+    else{
+        cout << "El numero se encuentra en la posicion " << pos + 1 << "\n";
+    }
     return 0;
+}
+
+int buscarA(int A[], int search, int high, int low){
+    int mid = (low + high) / 2, elemento;
+
+    if (low > high){
+        return -1;
+    }
+    else if(A[mid] == search){
+        return mid;
+    }
+    else if(search < A[mid]){
+        elemento = buscarA(A, search, mid - 1, low);
+        return elemento;
+    }
+    else if(search > A[mid]){
+        elemento = buscarA(A, search, high, mid + 1);
+        return elemento;
+    }
 }
